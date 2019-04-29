a71f98c3945b70214654e4df94cb3fa073d7a522
shicks1080 <stuart.hicks@gmail.com>
2019-04-29 13:37:33 +0200

    Edit to home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default () => (
       firstSlideCaption="2017 Skane 9's winners!"
       secondSlide={stags2018}
       secondSlideAlt="The Skane Stags rugby league team who travelled to Poland to meet their national team in 2018."
-      secondSlideCaption="Skane Stags v Poland 2018"
+      secondSlideCaption="Skane Stags Team ready to face Poland in 2018"
       thirdSlide={stagsPolandAction}
       thirdSlideAlt="Skane Stags rugby league team in action against Poland 2018."
       thirdSlideCaption="Action from the win against Poland."
@@ -32,13 +32,13 @@ export default () => (
   			Rugby League is a fast, physical and fun sport with a lively social side!
   		</p>
   		<p>
-  			Skåne Stags were established in 2013 and are the current national champions. We have a blend of nationalities with ex-pats from the likes of England, Australia and New Zealand joining local Swedish talent.
+  			Skåne Stags were established in 2013 and are the current national champions. We have a blend of nationalities with ex-pats from the likes of England, Australia, France and Tonga joining local Swedish talent.
   		</p>
   		<p>
-  			We can be found training at <a href="https://www.google.se/maps/place/Limhamnsf%C3%A4ltet/@55.5953031,12.9474745,323m/data=!3m1!1e3!4m5!3m4!1s0x4653a697bedc6367:0xc54fe103ad8766d!8m2!3d55.5953031!4d12.9489246?hl=en">Limhamnsfältet</a> in Malmö every Saturday from 10am during the season. Check our <a href="calender.php">calender</a> and <a href="https://www.facebook.com/skanestags">follow us on Facebook</a> for the latest schedule and news. All are welcome and talented players could make their way to the <a href="http://rugbyleague.se/">Swedish National Team Squad</a>.
+  			We can be found training at <a href="https://www.google.se/maps/place/Limhamnsf%C3%A4ltet/@55.5953031,12.9474745,323m/data=!3m1!1e3!4m5!3m4!1s0x4653a697bedc6367:0xc54fe103ad8766d!8m2!3d55.5953031!4d12.9489246?hl=en">Limhamnsfältet</a> in Malmö every Saturday from 10am during the season. Check our <a href="calender.php">calender</a> and <a href="https://www.facebook.com/skanestags">follow us on Facebook</a> for the latest schedule and news. All are welcome and talented players could make their way to the <a href="https://rugbyleague.se/">Swedish National Team Squad</a>.
   	  </p>
   		<p>
-  			Check out the highlights from our home 9's tournament in 2017 featuring <a href="https://www.facebook.com/KungsbackaRugbyClub/">Kungsbacka Broncos</a>, <a href="https://www.facebook.com/stockholm.rl/">Stockholm Kungar</a> and <a href="http://crlfc.dk/">Copenhagen</a>. Footage courtesy of our very own <a href="http://www.paulrhys.com/">Paul Rhys</a>.
+  			Check out the highlights from our home 9's tournament in 2017 featuring <a href="https://www.facebook.com/KungsbackaRugbyClub/">Kungsbacka Broncos</a>, <a href="https://www.facebook.com/stockholm.rl/">Stockholm Kungar</a> and <a href="https://crlfc.dk/">Copenhagen</a>. Footage courtesy of our very own <a href="https://www.paulrhys.com/">Paul Rhys</a>.
   		</p>
     </div>
     <Video
