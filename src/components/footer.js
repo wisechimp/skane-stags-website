78f1958b1cf7fe9ed7748337864af1cd88522e02
shicks1080 <stuart.hicks@gmail.com>
2019-04-29 13:34:55 +0200

    Completed the footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,14 @@
 import React from "react"
 
+import oLearysLogo from "../images/olearysnewlogo.jpg"
+
 export default () => (
-  <div>
-    <p>Footer shizzle</p>
+  <div className="footer">
+    <p>Supported by <a href="https://olearys.se/lund/" target="_blank" rel="nofollow noopener noreferrer">O'Leary's</a> in Lund.</p>
+    <img className="advert"
+      src={oLearysLogo}
+      alt="Logo for o'Learys in Lund, sponsors of Skåne Stags Rugby League Club."
+    />
+    <p>Join us there after the game!</p>
   </div>
 )
