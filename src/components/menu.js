fbc0aa80d697109f430aabc25c3a5056b2ca752b
shicks1080 <stuart.hicks@gmail.com>
2019-04-29 13:38:29 +0200

    Menu edited

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,12 +12,13 @@ export default () => (
         <Nav.Link href="/news/">News</Nav.Link>
         <NavDropdown title="Team" id="basic-nav-dropdown">
           <NavDropdown.Item href="/fixtures/">Fixtures</NavDropdown.Item>
+          <NavDropdown.Item href="/squad/">Squad</NavDropdown.Item>
           <NavDropdown.Item href="/training/">Training</NavDropdown.Item>
           <NavDropdown.Item href="/results/">Results</NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="About" id="basic-nav-dropdown">
           <NavDropdown.Item href="/about/">Skane Stags</NavDropdown.Item>
-          <NavDropdown.Item href="/rugby-league/">Rugby League</NavDropdown.Item>
+          <NavDropdown.Item href="/rugbyleague/">Rugby League</NavDropdown.Item>
           <NavDropdown.Item href="/partners/">Partners</NavDropdown.Item>
           <NavDropdown.Item href="/contact/">Contact</NavDropdown.Item>
         </NavDropdown>
