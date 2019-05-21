021958425e53b07ac49e5650cc10c1289b6c841d
shicks1080 <stuart.hicks@gmail.com>
2019-05-21 14:19:06 +0200

    Completed form with recaptcha (by reacptcha!).

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,98 +1,138 @@
-import React from "react"
+import React, { Component } from "react"
 import Axios from "axios"
+import firebase from "@firebase/app"
+import Reaptcha from "reaptcha"
 
 import styles from "./contactform.module.css"
-import Recaptcha from "./recaptcha"
 
-const config = {
-    apiKey: '****************', // Pull from .env
-    databaseURL: 'https://skane-stags-website.firebaseio.com'
-};
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactName: "",
+      email: "",
+      message: "",
+      buttonEnabled: false
+    };
+  }
 
-const handleSubmit = event => {
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
 
-  if (!firebaseApp && window !== undefined) {
-    const firebase = require('firebase/app');
-    setFirebaseApp(firebase.initializeApp(config));
-}
+    this.setState({
+      [name]: value,
+    })
+    console.log(this.state)
+  }
 
+  onVerify = () => {
+    this.setState({
+      buttonEnabled: true
+    });
+  }
+
+  handleSubmit = event => {
     event.preventDefault();
 
-    const data = { email, message, name, time: getTime() };
+    const config = {
+        apiKey: `${process.env.FIREBASE_API_KEY}`, // Pull from .env
+        databaseURL: 'https://skane-stags-website.firebaseio.com'
+    };
 
-    Axios.post('https://us-central1-skane-stags-website.cloudfunctions.net/submit', data).catch(error => {
-        console.log(error);
-    });
-};
+    if (!firebase) {
+      //const firebase = require('firebase/app');
+      firebase.initializeApp(config);
+  }
 
-export default () => (
-  <div className={styles.contactForm}>
-    <form onSubmit="{handleSubmit}">
-      <fieldset>
-        <div className={styles.formFields}>
-          <p className={styles.formRow}>
-            <label
-              className={styles.formLabel}
-              for="name"
-            >
-              Name:
-            </label>
-            <input
-              className={styles.formInput}
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Your name"
-              required="required"
-              value="{name}"
-              onChange="{handleNameChange}"
-            />
-          </p>
-          <p className={styles.formRow}>
-            <label
-              className={styles.formLabel}
-              for="email"
-            >
-              Email:
-            </label>
-            <input
-              className={styles.formInput}
-              type="email"
-              id="email"
-              email="email"
-              placeholder="yourname@email.com"
-              required="required"
-              value="{email}"
-              onChange="{handleEmailChange}"
-            />
-          </p>
-          <p className={styles.formRow}>
-            <label
-              className={styles.formLabel}
-              for="message"
-            >
-              Message:
-            </label>
-            <textarea
-              className={styles.formInput}
-              id="message"
-              message="message"
-              placeholder="Your message"
-              required="required"
-              rows="5"
-              value="{message}"
-              onChange="{handleMessageChange}"
-            />
-          </p>
-          <Recaptcha />
-          <div className={styles.formButton}>
-            <input
-              type="submit"
-              value="Send message"
-            />
-          </div>
-        </div>
-      </fieldset>
-    </form>
-  </div>
-)
+      const email = this.state.email
+      const message = this.state.message
+      const contactName = this.state.contactName
+      const data = { email, message, contactName }
+      console.log(data)
+
+      Axios.post('https://us-central1-skane-stags-website.cloudfunctions.net/submit', data).catch(error => {
+          console.log("What an error:" + error);
+      });
+  };
+
+  render() {
+    return (
+      <div className={styles.contactForm}>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <div className={styles.formFields}>
+              <p className={styles.formRow}>
+                <label
+                  className={styles.formLabel}
+                  htmlFor="contactName"
+                >
+                  Name:
+                </label>
+                <input
+                  className={styles.formInput}
+                  type="text"
+                  id="contactName"
+                  name="contactName"
+                  placeholder="Your name"
+                  required="required"
+                  value={this.state.contactName}
+                  onChange={this.handleInputChange}
+                />
+              </p>
+              <p className={styles.formRow}>
+                <label
+                  className={styles.formLabel}
+                  htmlFor="email"
+                >
+                  Email:
+                </label>
+                <input
+                  className={styles.formInput}
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="yourname@email.com"
+                  required="required"
+                  value={this.state.email}
+                  onChange={this.handleInputChange}
+                />
+              </p>
+              <p className={styles.formRow}>
+                <label
+                  className={styles.formLabel}
+                  htmlFor="message"
+                >
+                  Message:
+                </label>
+                <textarea
+                  className={styles.formInput}
+                  id="message"
+                  name="message"
+                  placeholder="Your message"
+                  required="required"
+                  rows="5"
+                  value={this.state.message}
+                  onChange={this.handleInputChange}
+                />
+              </p>
+              <Reaptcha
+                sitekey={`${process.env.RECAPTCHA_SITE_KEY}`}
+                onVerify={this.onVerify}
+              />
+              <button className={styles.formButton}
+                id="submitButton"
+                disabled={!this.state.buttonEnabled}
+              >
+                Send message
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ContactForm
