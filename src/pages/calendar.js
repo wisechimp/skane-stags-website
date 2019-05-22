8093555cd67c8342ee14bc85d822a40747b39524
shicks1080 <stuart.hicks@gmail.com>
2019-05-22 16:38:01 +0200

    Altering the styling for the Calendar to give it a min height so that the events don't become hidden.

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,16 +1,18 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Video from "../components/video"
 
 export default () => (
   <Layout>
     <h1>Calendar</h1>
-    <Video
-      title="Skane Stags Rugby League Club Calendar"
-      width="800"
-      height="600"
-      src="https://calendar.google.com/calendar/embed?src=skanerugbyleague%40gmail.com&ctz=Europe/Stockholm"
-    />
+    <div className="calendarWrapper">
+      <iframe
+        title="Skane Stags Rugby League Club Calendar"
+        width="800"
+        height="600"
+        src="https://calendar.google.com/calendar/embed?src=skanerugbyleague%40gmail.com&ctz=Europe/Stockholm"
+        frameBorder="0"
+      />
+    </div>
   </Layout>
 )
