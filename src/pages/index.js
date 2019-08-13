b0b75616c06e13ab1883bb481ccf45304fb2b37d
shicks1080 <stuart.hicks@gmail.com>
2019-08-13 16:28:15 +0200

    Adding the training photo to the carousel on the home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import HomeTitle from "../components/hometitle"
 import Layout from "../components/layout"
 import Video from "../components/video"
 import stagsLogo from "../images/skanestagslogo.png"
+import stagstraining2019 from "../images/2019stagsgenerictraining.jpg"
 import stags2017 from "../images/2017skanestagslund9steam.jpg"
 import stags2018 from "../images/2018skanestagspolandteam.jpg"
 import stagsPolandAction from "../images/2018skanestagspolandaction.jpg"
@@ -29,26 +30,32 @@ export default () => (
         imgSrc={stagsLogo}
         imgAlt="The logo of Skane Stags Rugby League club"/>
       <ImageCarousel
-        firstSlide={stags2017}
-        firstSlideAlt="Skane Stags Rugby League team after winning the 2017 Skane 9\'s tournament"
-        firstSlideCaption="2017 Skane 9's winners!"
+        firstSlide={stagstraining2019}
+        firstSlideAlt="Picture of Skane Stags Rugby League being briefed during a trainng session. Malmö's turning tower can be seen in the background"
+        firstSlideCaption="Join us on Saturday mornings in Malmö!"
         secondSlide={stags2018}
         secondSlideAlt="The Skane Stags rugby league team who travelled to Poland to meet their national team in 2018."
         secondSlideCaption="Skane Stags Team ready to face Poland in 2018"
         thirdSlide={stagsPolandAction}
         thirdSlideAlt="Skane Stags rugby league team in action against Poland 2018."
         thirdSlideCaption="Action from the win against Poland."
+        fourthSlide={stags2017}
+        fourthSlideAlt="Skane Stags Rugby League team after winning the 2017 Skane 9\'s tournament"
+        fourthSlideCaption="2017 Skane 9's winners!"
       />
       <div className="bodyContent">
         <p>
     			Rugby League is a fast, physical and fun sport with a lively social side!
     		</p>
+        <br />
     		<p>
     			Skåne Stags were established in 2013 and are the current national champions. We have a blend of nationalities with ex-pats from the likes of England, Australia, France and Tonga joining local Swedish talent.
     		</p>
+        <br />
     		<p>
     			We can be found training at <a href="https://www.google.se/maps/place/Limhamnsf%C3%A4ltet/@55.5953031,12.9474745,323m/data=!3m1!1e3!4m5!3m4!1s0x4653a697bedc6367:0xc54fe103ad8766d!8m2!3d55.5953031!4d12.9489246?hl=en">Limhamnsfältet</a> in Malmö every Saturday from 10am during the season. Check our <Link to="/calendar">calendar</Link> and <a href="https://www.facebook.com/skanestags">follow us on Facebook</a> for the latest schedule and news. All are welcome and talented players could make their way to the <a href="https://www.facebook.com/swedenrugbyleague/">Swedish National Team Squad</a>.
     	  </p>
+        <br />
     		<p>
     			Check out the highlights from our home 9's tournament in 2017 featuring <a href="https://www.facebook.com/KungsbackaRugbyClub/">Kungsbacka Broncos</a>, <a href="https://www.facebook.com/stockholm.rl/">Stockholm Kungar</a> and <a href="https://www.facebook.com/CopenhagenRlfc/">Copenhagen</a>. Footage courtesy of our very own <a href="https://www.paulrhys.com/">Paul Rhys</a>.
     		</p>
