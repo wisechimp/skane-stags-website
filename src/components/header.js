ff9ee3801c2df2f92276ba57c1bfed913701d1a3
shicks1080 <stuart.hicks@gmail.com>
2021-01-25 10:47:56 +0100

    Logo work
    Making the icon transparent.
    Correcting the reference in the header to the apple icon.
    Replacing the icon with one without a white background.

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,32 @@ export default () => (
     <html lang="en" />
     <title>Skane Stags Rugby League</title>
     <meta charset="utf-8" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-    <meta name="description" content="The website for Skane Stags Rugby League club" />
+    <meta
+      name="apple-mobile-web-app-status-bar-style"
+      content="black-translucent"
+    />
+    <meta
+      name="description"
+      content="The website for Skane Stags Rugby League club"
+    />
     <meta name="keywords" content="rugby league, skane" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta property="og:title" content="Skane Stags Rugby League Club" />
-    <meta property="og:description" content="Skane Stags are a rugby league club based in Skåne in southern Sweden." />
-    <meta property="og:image" content="https://skanestags.com/skanestagslogo.png" />
+    <meta
+      property="og:description"
+      content="Skane Stags are a rugby league club based in Skåne in southern Sweden."
+    />
+    <meta
+      property="og:image"
+      content="https://skanestags.com/skanestagslogo.png"
+    />
     <meta property="og:url" content="https://skanestags.com/index.htm" />
     <meta property="og:site_name" content="Skåne Stags Rugby League Club" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image:alt" content="Skane Stags Rugby League Club logo." />
-    <link rel="apple-touch-icon" href="single-page-icon.png" />
+    <meta
+      name="twitter:image:alt"
+      content="Skane Stags Rugby League Club logo."
+    />
+    <link rel="apple-touch-icon" href="apple-touch-icon.png" />
   </Helmet>
 )
