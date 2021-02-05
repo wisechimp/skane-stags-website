bd7c21cc453e5fe54a7785654d79a5fee27f463f
shicks1080 <stuart.hicks@gmail.com>
2021-02-05 10:31:14 +0100

    Update contactform.js
    Redefining recaptcha key.

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -4,10 +4,10 @@ import Recaptcha from "react-google-recaptcha"
 
 import contactformStyles from "./contactform.module.css"
 
-const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY
+const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
 if (typeof RECAPTCHA_KEY === "undefined") {
   throw new Error(`
-    Env var SITE_RECAPTCHA_KEY is undefined! 
+    Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
     You probably forget to set it in your Netlify build environment variables. 
     Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
     Note this demo is specifically for Recaptcha v2
