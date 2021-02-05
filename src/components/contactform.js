f7d0137ec3f8b6357f4734269fe47279036dd294
shicks1080 <stuart.hicks@gmail.com>
2021-02-05 11:52:20 +0100

    Update contactform.js
    Bringing the external recaptcha back.

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,17 +1,18 @@
 import React, { useState, createRef } from "react"
 import { navigate } from "gatsby"
+import Recaptcha from "react-google-recaptcha"
 
 import contactformStyles from "./contactform.module.css"
 
-/* const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
+const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY
 if (typeof RECAPTCHA_KEY === "undefined") {
   throw new Error(`
-    Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
+    Env var GATSBY_SITE_RECAPTCHA_KEY is undefined! 
     You probably forget to set it in your Netlify build environment variables. 
     Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
     Note this demo is specifically for Recaptcha v2
     `)
-} */
+}
 
 function encode(data) {
   return Object.keys(data)
@@ -55,7 +56,6 @@ export default () => {
         action="/thanks/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        data-netlify-recaptcha="true"
         onSubmit={handleSubmit}
       >
         <noscript>
@@ -114,7 +114,7 @@ export default () => {
                 onChange={handleChange}
               />
             </p>
-            <div data-netlify-recaptcha="true"></div>
+            <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
             <button className={contactformStyles.formButton} type="submit">
               Send Message
             </button>
