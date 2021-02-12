cf2dcd9a87ce6da4d2d902bcc71f5aa96615c19e
shicks1080 <stuart.hicks@gmail.com>
2021-02-12 15:45:04 +0100

    Making the image in the footer responsive
    Also providing webp as a first option.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,58 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-import styles from "./footer.module.css"
-import oLearysLogo from "../images/olearysnewlogo.jpg"
+import { footer, footerLogo } from "./footer.module.css"
 
-export default () => (
-  <div className={styles.footer}>
-    <p>Supported by <a href="https://olearys.se/lund/" target="_blank" rel="nofollow noopener noreferrer">O'Leary's</a> in Lund.</p>
-    <img className={styles.footerLogo}
-      src={oLearysLogo}
-      alt="Logo for o'Learys in Lund, sponsors of Skåne Stags Rugby League Club."
-    />
-    <p>Join us there after the game!</p>
-  </div>
-)
+export default () => {
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      mobileFooterImage: file(relativePath: { eq: "olearysnewlogo.jpg" }) {
+        childImageSharp {
+          fixed(width: 200, quality: 100) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      desktopFooterImage: file(relativePath: { eq: "olearysnewlogo.jpg" }) {
+        childImageSharp {
+          fixed(width: 500, quality: 100) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+    }
+  `)
+
+  const { mobileFooterImage, desktopFooterImage } = data
+  const sources = [
+    mobileFooterImage.childImageSharp.fixed,
+    {
+      ...desktopFooterImage.childImageSharp.fixed,
+      media: `(min-width: 769px)`,
+    },
+  ]
+
+  return (
+    <div className={footer}>
+      <p>
+        Supported by{" "}
+        <a
+          href="https://olearys.se/lund/"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          O'Leary's
+        </a>{" "}
+        in Lund.
+      </p>
+      <br />
+      <Img
+        className={footerLogo}
+        fixed={sources}
+        alt="Logo for o'Learys in Lund, sponsors of Skåne Stags Rugby League Club."
+      />
+      <p>Join us there after the game!</p>
+    </div>
+  )
+}
