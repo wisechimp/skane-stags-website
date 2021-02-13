74ce7284f75b1574c13aa77f178e21fcb9372cce
shicks1080 <stuart.hicks@gmail.com>
2021-02-13 11:20:33 +0100

    Training page - Making the image responsive and adding webp format

--- src/pages/training.js
+++ src/pages/training.js
@@ -1,21 +1,55 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Map from "../components/map"
-import TrainingPoster from "../images/2019stagstrainingen.png"
 
-export default () => (
-  <Layout pageTitle="Training">
-    <img
-      className="poster"
-      src={TrainingPoster}
-      alt="Training poster for the Skane Stags"
-    />
-    <Map
-      title="Limhansfältet, Skane Stags Rugby League Club's usual training location"
-      width="800"
-      height="600"
-      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2938.420870229325!2d12.949971661712082!3d55.59596972092554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sse!4v1556632352524!5m2!1sen!2sse"
-    />
-  </Layout>
-)
+export default ({ data }) => {
+  const { mobileTrainingPoster, desktopTrainingPoster } = data
+  const sources = [
+    mobileTrainingPoster.childImageSharp.fluid,
+    {
+      ...desktopTrainingPoster.childImageSharp.fluid,
+      media: `(min-width: 769px)`,
+    },
+  ]
+  return (
+    <Layout pageTitle="Training">
+      <Img
+        className="poster"
+        fluid={sources}
+        alt="Training poster for the Skane Stags"
+      />
+      <Map
+        title="Limhansfältet, Skane Stags Rugby League Club's usual training location"
+        width="800"
+        height="600"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2938.420870229325!2d12.949971661712082!3d55.59596972092554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sse!4v1556632352524!5m2!1sen!2sse"
+      />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    mobileTrainingPoster: file(
+      relativePath: { eq: "2019stagstrainingen.png" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 800, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    desktopTrainingPoster: file(
+      relativePath: { eq: "2019stagstrainingen.png" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 2000, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
