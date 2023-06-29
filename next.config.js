a390d392d6daf9cf8a8eceeeec739ff1987ba635
shicks1080 <stuart.hicks@gmail.com>
2023-06-29 08:51:45 +0200

    housekeeping: Adding Next plus configs
    Removing all the Gatsby specific packages and adding the first Next ones.

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
