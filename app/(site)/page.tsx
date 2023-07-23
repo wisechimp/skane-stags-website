3dc738859b4ef4d8b04c658f5ac742d0f4fef83c
shicks1080 <stuart.hicks@gmail.com>
2023-07-23 14:12:44 +0200

    feat: Adding Latest News to the home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,14 +1,26 @@
-import { getPage } from "@/sanity/sanity-utils"
+import { getNewsItems, getPage } from "@/sanity/sanity-utils"
 
 import Jumbotron from "@/app/components/jumbotron/Jumbotron"
+import NewsCard from "../components/newscard/NewsCard"
+import * as styles from './home.module.css'
 
 const Home = async () => {
   const page = await getPage('home')
   const { content, mainImage, mainImageAlt } = page
+  const latestNews = await (await getNewsItems()).slice(0, 3)
+  console.log(latestNews)
 
   return (
     <div>
       <Jumbotron image={mainImage} imageAlt={mainImageAlt} content={content} />
+      <div className={styles.latestNewsContainer}>
+        <h2>Latest News</h2>
+        <div className={styles.latestNewsCardsContainer}>
+          {latestNews.map(latestNewsItem => (
+            <NewsCard newsItem={latestNewsItem} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
