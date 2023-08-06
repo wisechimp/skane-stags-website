c63f240aba2dcc54d722821b9337a13abe002ac9
shicks1080 <stuart.hicks@gmail.com>
2023-08-06 12:55:46 +0200

    feat: Adding accessibility labels to the social media buttons

--- app/components/menus/SocialMenu.tsx
+++ app/components/menus/SocialMenu.tsx
@@ -13,16 +13,16 @@ const SocialMenu = () => {
     <div className={styles.socialMenuContainer}>
       <Link href="/contact">Contact</Link>
       <div className={styles.socialMenuIcons}>
-        <Link href="https://www.facebook.com/skanestags/">
+        <Link href="https://www.facebook.com/skanestags/" aria-label="Visit our Facebook page">
           <FaFacebookSquare />
         </Link>
-        <Link href="https://www.instagram.com/skanestags">
+        <Link href="https://www.instagram.com/skanestags" aria-label="Visit our Instagram feed">
           <FaInstagram />
         </Link>
-        <Link href="https://twitter.com/skanecrusaders">
+        <Link href="https://twitter.com/skanecrusaders" aria-label="We're on Twitter too">
           <FaTwitter />
         </Link>
-        <Link href="https://www.youtube.com/channel/UCFgK9OchuqAxrBLrvcs4MKg">
+        <Link href="https://www.youtube.com/channel/UCFgK9OchuqAxrBLrvcs4MKg" aria-label="We occassionally post videos to YouTube">
           <FaYoutube />
         </Link>
       </div>
